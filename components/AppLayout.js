025dae07213c2cf2e084c568b07e9fb6f29163fc
star <chlvkfksquf@gmail.com>
2022-03-29 19:03:45 +0900

    Add AppLayout Link

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,16 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
 
+// eslint-disable-next-line react/prop-types
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
+      <div>
+        <Link href="/">
+          <a>노드버드</a>
+        </Link>
+        <Link href="/profile">
+          <a>프로필</a>
+        </Link>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
       {children}
     </div>
   );
 };
 
-AppLayout.prototype = {
+AppLayout.propsTypes = {
   children: PropTypes.node.isRequired,
 };
 
