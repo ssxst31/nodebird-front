c3ab22a0e97e340625e0531e2c70b36f9e8240a7
star <chlvkfksquf@gmail.com>
2022-03-29 19:04:30 +0900

    Add AppLayout

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,8 @@
 import React from "react";
+import AppLayout from "../components/AppLayout";
 
 const Profile = () => {
-  return <div>내 프로필</div>;
+  return <AppLayout>내 프로필</AppLayout>;
 };
 
 export default Profile;
