2a5eb3cc2abbe7c5edeb685d369b2ba809cb8136
star <chlvkfksquf@gmail.com>
2022-03-31 01:44:53 +0900

    Add next head

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,17 @@
 import React from "react";
 import AppLayout from "../components/AppLayout";
+import Head from "next/head";
 
 const Profile = () => {
-  return <AppLayout>내 프로필</AppLayout>;
+  return (
+    <AppLayout>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      내 프로필
+    </AppLayout>
+  );
 };
 
 export default Profile;
--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,17 @@
 import React from "react";
 import AppLayout from "../components/AppLayout";
+import Head from "next/head";
 
 const Signup = () => {
-  return <AppLayout>회원가입 페이지</AppLayout>;
+  return (
+    <AppLayout>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      회원가입 페이지
+    </AppLayout>
+  );
 };
 
 export default Signup;
