c3079f9e0122f36da29dd8bfa404fdb1a99c0645
star <chlvkfksquf@gmail.com>
2022-03-31 02:35:01 +0900

    Add AppLayout inputs

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,29 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
+import LoginForm from "./LoginForm";
+import UserProfile from "./UserProfile";
 
 // eslint-disable-next-line react/prop-types
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
-        <Menu.Item>
+        <Menu.Item key={1}>
           <Link href="/">
             <a>노드버드</a>
           </Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key={2}>
           <Link href="/profile">
             <a>프로필</a>
           </Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key={3}>
+          <Input.Search
+            enterButton
+            style={{ verticalAlign: "middle" }}
+            onClick={setIsLoggedIn}
+          />
+        </Menu.Item>
+        <Menu.Item key={4}>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={6}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://github.com/ssxst31"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Made by ssxst31
+          </a>
+        </Col>
+      </Row>
       {children}
     </div>
   );
