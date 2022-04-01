494f48c927a5d0f9daeff571ea5e83b2c3bddb4a
star <chlvkfksquf@gmail.com>
2022-04-01 13:33:29 +0900

    Add LoginForm propTypes

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from "react";
 import { Form, Input, Button } from "antd";
 import Link from "next/link";
 import styled from "styled-components";
+import propTypes from "prop-types";
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -58,6 +59,10 @@ const LoginForm = ({ setIsLoggedIn }) => {
       </ButtonWrapper>
     </FormWrapper>
   );
+};
+
+LoginForm.propTypes = {
+  setIsLoggedIn: propTypes.any.isRequired,
 };
 
 export default LoginForm;
