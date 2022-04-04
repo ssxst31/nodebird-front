253dea80824304415dd6e2cb51ca1557d28c3653
star <chlvkfksquf@gmail.com>
2022-04-04 21:05:52 +0900

    Add PostImage component

---
+++ components/PostImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import propTypes from "prop-types";
+
+const PostImages = ({ images }) => {
+  return <div>구현중...</div>;
+};
+
+PostImages.propTypes = {
+  images: propTypes.arrayOf(propTypes.object),
+};
+
+export default PostImages;
