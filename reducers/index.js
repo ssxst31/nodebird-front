420c26df55773698367ed15a9fb0ca7db52caf99
star <chlvkfksquf@gmail.com>
2022-04-04 18:26:34 +0900

    Modify index reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,50 +1,27 @@
 import { HYDRATE } from "next-redux-wrapper";
+import { combineReducers } from "redux";
+import user from "./user";
+import post from "./post";
 
 const initialState = {
-  user: {
-    isLoggedIn: false,
-    user: null,
-    signUpData: {},
-    loginData: {},
-  },
-  post: {
-    mainPosts: [],
-  },
-};
+  user: {},
 
-// action creator
-export const loginAction = (data) => {
-  return {
-    type: "LOG_IN",
-    data,
-  };
-};
-
-export const logoutAction = () => {
-  return {
-    type: "LOG_OUT",
-  };
+  post: {},
 };
 
 // (이전상태, 액션) => 다음상태
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      console.log("HYDRATE", action);
-      return { ...state, ...action.payload };
-    case "LOG_IN":
-      return {
-        ...state,
-        user: { ...state.user, isLoggedIn: true, user: action.data },
-      };
-    case "LOG_OUT":
-      return {
-        ...state,
-        user: { ...state.user, isLoggedIn: false, user: null },
-      };
-    default:
-      return state;
-  }
-};
+const reducer = combineReducers({
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        console.log("HYDRATE", action);
+        return { ...state, ...action.payload };
+      default:
+        return state;
+    }
+  },
+  user,
+  post,
+});
 
 export default reducer;
