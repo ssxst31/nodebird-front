7929e440dcee442a041cb01d30a97a0038b904c5
star <chlvkfksquf@gmail.com>
2022-04-13 02:00:02 +0900

    Add CommentForm layout

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -1,8 +1,42 @@
-import React from "react";
-import { Form } from "antd";
+import React, { useCallback } from "react";
+
+import { useSelector } from "react-redux";
+import { Form, Input, Button } from "antd";
+import propTypes from "prop-types";
+
+import useInput from "../hooks/useInput";
 
 const CommentForm = ({ post }) => {
-  return <Form>폼</Form>;
+  const id = useSelector((state) => {
+    state.user.me?.id;
+  });
+  const [commentText, onChangeCommentText] = useInput("");
+  const onSubmitComment = useCallback(() => {
+    console.log(post.id, commentText);
+  }, [commentText]);
+
+  return (
+    <Form onFinish={onSubmitComment}>
+      <Form.Item style={{ position: "relative" }}>
+        <Input.TextArea
+          value={commentText}
+          onChange={onChangeCommentText}
+          rows={4}
+        />
+        <Button
+          style={{ position: "absolute", right: 0, bottom: -40 }}
+          type="primary"
+          htmlType="submit"
+        >
+          삐약
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+CommentForm.propTypes = {
+  post: propTypes.object.isRequired,
 };
 
 export default CommentForm;
