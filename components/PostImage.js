1e2892e5110c98bd4e6b8d39373699cea380ce55
star <chlvkfksquf@gmail.com>
2022-04-14 17:56:27 +0900

    Add PostImages layout

--- components/PostImage.js
+++ components/PostImage.js
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import propTypes from "prop-types";
+import { PlusOutlined } from "@ant-design/icons";
 
 const PostImages = ({ images }) => {
-  return <div>구현중...</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        ></img>
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          width="50%"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          width="50%"
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
+        <img
+          role="presentation"
+          width="50%"
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+        <div
+          role="presentation"
+          style={{
+            display: "inline-block",
+            width: "56%",
+            textAlign: "center",
+            verticalAlign: "middle",
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}개의 사진
+        </div>
+      </div>
+    </>
+  );
 };
 
 PostImages.propTypes = {
