0f72d46da7780830b89484ee561f7370c9806e40
star <chlvkfksquf@gmail.com>
2022-04-20 03:17:33 +0900

    loggerMiddleware

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,10 +1,17 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import reducer from "../reducers";
 
+import reducer from "../reducers";
+const loggerMiddleware =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action);
+    return next(action);
+  };
 const configureStore = () => {
-  const middlewares = [];
+  const middlewares = [loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
