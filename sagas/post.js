0dbfef9c9f0d5a1568a9ecb09b39eb61cca92aab
star <chlvkfksquf@gmail.com>
2022-05-05 02:52:26 +0900

    Add postSaga

---
+++ sagas/post.js
@@ -0,0 +1,24 @@
+import { all, fork, takeLatest, delay, put } from "redux-saga/effects";
+import axios from "axios";
+
+function addPostAPI() {
+  return axios.post("/api/post");
+}
+
+function* addPost() {
+  try {
+    // const result = yield call(addPostAPI);
+    yield delay(1000);
+    yield put({ type: "ADD_POST_SUCCESS" });
+  } catch (err) {
+    yield put({ type: "ADD_POST_FAILURE", data: err.response.data });
+  }
+}
+
+function* watchAddPost() {
+  yield takeLatest("ADD_POST_REQUEST", addPost);
+}
+
+export default function* postSaga() {
+  yield all([fork(watchAddPost)]);
+}
