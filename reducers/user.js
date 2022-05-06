2a9036a09ace99221705bf5711fb2f09c0b18f38
star <chlvkfksquf@gmail.com>
2022-05-06 23:22:40 +0900

    Add reducer user isLogging

--- reducers/user.js
+++ reducers/user.js
@@ -1,5 +1,7 @@
 export const initialState = {
+  isLoggingIn: false, // 로그인 시도중
   isLoggedIn: false,
+  isLoggingOut: false, // 로그아웃 시도중
   me: null,
   signUpData: {},
   loginData: {},
@@ -49,10 +51,34 @@ export const logoutFailureAction = (data) => {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "LOG_IN":
-      return { ...state.user, isLoggedIn: true, me: action.data };
-    case "LOG_OUT":
-      return { ...state.user, isLoggedIn: false, me: null };
+    case "LOG_IN_REQUEST":
+      return {
+        ...state.user,
+        isLoggingIn: true,
+      };
+    case "LOG_IN_SUCCESS":
+      return {
+        ...state.user,
+        isLoggingIn: true,
+        isLoggedIn: true,
+        me: action.data,
+      };
+    case "LOG_IN_FAILURE":
+      return { ...state.user, isLoggingIn: true, isLoggedIn: true };
+    case "LOG_OUT_REQUEST":
+      return { ...state.user, isLoggingOut: true };
+    case "LOG_OUT_SUCCESS":
+      return {
+        ...state.user,
+        isLoggingOut: false,
+        isLoggedOut: false,
+        me: null,
+      };
+    case "LOG_OUT_FAILURE":
+      return {
+        ...state.user,
+        isLoggingOut: false,
+      };
     default:
       return state;
   }
