d72c8589e6003306b5dd795481b4b8ae9141e0fa
star <chlvkfksquf@gmail.com>
2022-05-17 23:04:17 +0900

    Add saga post watchSignUp

--- sagas/user.js
+++ sagas/user.js
@@ -1,5 +1,16 @@
 import { all, fork, takeLatest, delay, put } from "redux-saga/effects";
 import axios from "axios";
+import {
+  LOG_IN_SUCCESS,
+  LOG_OUT_SUCCESS,
+  LOG_OUT_REQUEST,
+  LOG_OUT_FAILURE,
+  LOG_IN_REQUEST,
+  LOG_IN_FAILURE,
+  SIGN_UP_FAILURE,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_REQUEST,
+} from "../reducers/user";
 
 function logInAPI(data) {
   return axios.post("/api/login", data);
@@ -9,9 +20,9 @@ function* logIn(action) {
   try {
     // const result = yield call(logInAPI, action.data); // fork 비동기 함수 호출 call 동기 함수 호출 첫번째 자리는 함수 나머지는 매개변수 call의 특성
     yield delay(1000);
-    yield put({ type: "LOG_IN_SUCCESS", data: action.data });
+    yield put({ type: LOG_IN_SUCCESS, data: action.data });
   } catch (err) {
-    yield put({ type: "LOG_IN_FAILURE", data: err.response.data });
+    yield put({ type: LOG_IN_FAILURE, error: err.response.data });
   }
 }
 
@@ -23,18 +34,36 @@ function* logout() {
   try {
     // const result = yield call(logOutAPI);
     yield delay(1000);
-    yield put({ type: "LOG_OUT_SUCCESS" });
+    yield put({ type: LOG_OUT_SUCCESS });
   } catch (err) {
-    yield put({ type: "LOG_OUT_FAILURE", data: err.response.data });
+    yield put({ type: LOG_OUT_FAILURE, error: err.response.data });
+  }
+}
+
+function signUpAPI(data) {
+  return axios.post("/api/signUp");
+}
+
+function* signUp(action) {
+  try {
+    // const result = yield call(signUpAPI);
+    yield delay(1000);
+    yield put({ type: SIGN_UP_SUCCESS });
+  } catch (err) {
+    yield put({ type: SIGN_UP_FAILURE, error: err.response.data });
   }
 }
 
 function* watchLogIn() {
-  yield takeLatest("LOG_IN_REQUEST", logIn);
+  yield takeLatest(LOG_IN_REQUEST, logIn);
 }
 
 function* watchLogOut() {
-  yield takeLatest("LOG_OUT_REQUEST", logout);
+  yield takeLatest(LOG_OUT_REQUEST, logout);
+}
+
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga() {
