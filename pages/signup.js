57245e7f98315c0da06fc00303b7fd85e42798f1
star <chlvkfksquf@gmail.com>
2022-05-19 17:25:03 +0900

    Add siginup signUpLoading

--- pages/signup.js
+++ pages/signup.js
@@ -4,13 +4,20 @@ import Head from "next/head";
 import { Form, Input, Checkbox, Button } from "antd";
 import useInput from "../hooks/useInput";
 import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { SIGN_UP_REQUEST } from "../reducers/user";
 
 const ErrorMessage = styled.div`
   color: red;
 `;
 
 const Signup = () => {
-  const [id, onChangeId] = useInput("");
+  const dispatch = useDispatch();
+  const { signUpLoading } = useSelector((state) => {
+    state.user;
+  });
+
+  const [email, onChangeEmail] = useInput("");
   const [nickname, onChangeNickname] = useInput("");
   const [password, onChangePassword] = useInput("");
   const [passwordCheck, setPasswordCheck] = useState("");
@@ -38,7 +45,8 @@ const Signup = () => {
     if (!term) {
       return setTermError(true);
     }
-    console.log(id, nickname, password, passwordCheck);
+    console.log(email, nickname, password, passwordCheck);
+    dispatch({ type: SIGN_UP_REQUEST, data: { email, nickname, password } });
   }, [password, passwordCheck, term]);
 
   return (
@@ -49,15 +57,21 @@ const Signup = () => {
       </Head>
       <Form onFinish={onSubmit}>
         <div>
-          <label htmlFor="user-id">아이디</label>
-          <br />
-          <Input name="user-id" value={id} required onChange={onChangeId} />
-        </div>
-        <div>
-          <label htmlFor="user-id">닉네임</label>
+          <label htmlFor="user-email">이메일</label>
           <br />
           <Input
-            name="user-id"
+            name="user-email"
+            type="email"
+            value={email}
+            required
+            onChange={onChangeEmail}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-nick">닉네임</label>
+          <br />
+          <Input
+            name="user-nick "
             value={nickname}
             required
             onChange={onChangeNickname}
@@ -95,7 +109,7 @@ const Signup = () => {
           {termError && <ErrorMessage>약관에 동의하셔야 합니다.</ErrorMessage>}
         </div>
         <div style={{ marginTop: 10 }}>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" htmlType="submit" loading={signUpLoading}>
             가입하기
           </Button>
         </div>
