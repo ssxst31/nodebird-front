337867a2a3106dd2c64114942ec5d2d37d5c41a1
star <chlvkfksquf@gmail.com>
2022-05-19 17:11:35 +0900

    Add CommentForm dispatch

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -1,18 +1,24 @@
 import React, { useCallback } from "react";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Form, Input, Button } from "antd";
 import propTypes from "prop-types";
 
 import useInput from "../hooks/useInput";
+import { ADD_COMMENT_REQUEST } from "../reducers/post";
 
 const CommentForm = ({ post }) => {
+  const dispatch = useDispatch();
   const id = useSelector((state) => {
     state.user.me?.id;
   });
   const [commentText, onChangeCommentText] = useInput("");
   const onSubmitComment = useCallback(() => {
     console.log(post.id, commentText);
+    dispatch({
+      type: ADD_COMMENT_REQUEST,
+      data: { content: commentText, postId: post.id, userId: id },
+    });
   }, [commentText]);
 
   return (
