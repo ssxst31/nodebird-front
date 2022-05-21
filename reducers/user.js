68b73d787e768c6b1d6c546449eba0ba40fdfaa5
star <chlvkfksquf@gmail.com>
2022-05-22 00:24:41 +0900

    Fix reducers user name

--- reducers/user.js
+++ reducers/user.js
@@ -16,9 +16,9 @@ export const initialState = {
   loginData: {},
 };
 
-export const LOG_IN_REQUEST = "LOG_IN_Request";
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST";
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
-export const LOG_IN_FAILURE = "LOG_IN_Request";
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
 
 export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
 export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
@@ -74,7 +74,7 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         logInLoading: false,
-        logInError: true,
+        logInDone: true,
         me: dummyUser(action.data),
       };
     case LOG_IN_FAILURE:
